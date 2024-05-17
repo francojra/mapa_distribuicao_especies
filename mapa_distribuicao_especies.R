b64215f04cabf25b4a19cb4fe42f4b64a253f33a
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2024-05-17 01:34:42 -0300

    Add files via upload

--- mapa_distribuicao_especies.R
+++ mapa_distribuicao_especies.R
@@ -26,6 +26,12 @@ occ_data1 <- occ_search(scientificName = species_name1, limit = 500)
 species_name2 <- c("Gymnogyps californianus")
 occ_data2 <- occ_search(scientificName = species_name2, limit = 500)
 
+species_name3 <- c("Oncorhynchus nerka")
+occ_data3 <- occ_search(scientificName = species_name3, limit = 500)
+
+species_name4 <- c("Canis rufus")
+occ_data4 <- occ_search(scientificName = species_name4, limit = 500)
+
 # Extrair coordenadas
 
 coords <- occ_data$data %>%
@@ -37,6 +43,14 @@ coords1 <- occ_data1$data %>%
   select(decimalLongitude, decimalLatitude)
 
 coords2 <- occ_data2$data %>%
+  filter(!is.na(decimalLongitude) & !is.na(decimalLatitude)) %>%
+  select(decimalLongitude, decimalLatitude)
+
+coords3 <- occ_data3$data %>%
+  filter(!is.na(decimalLongitude) & !is.na(decimalLatitude)) %>%
+  select(decimalLongitude, decimalLatitude)
+
+coords4 <- occ_data4$data %>%
   filter(!is.na(decimalLongitude) & !is.na(decimalLatitude)) %>%
   select(decimalLongitude, decimalLatitude)
 
@@ -54,6 +68,14 @@ coords_sf2 <- st_as_sf(coords2, coords = c("decimalLongitude",
                                          "decimalLatitude"), 
                       crs = 4326)
 
+coords_sf3 <- st_as_sf(coords3, coords = c("decimalLongitude", 
+                                         "decimalLatitude"), 
+                      crs = 4326)
+
+coords_sf4 <- st_as_sf(coords4, coords = c("decimalLongitude", 
+                                         "decimalLatitude"), 
+                      crs = 4326)
+
 # Obter dados das fronteiras dos países
 
 world <- ne_countries(scale = "medium", returnclass = "sf")
@@ -64,8 +86,8 @@ america <- world %>%
   filter(continent %in% c("North America")) 
 
 # Ajustar os limites do mapa para focar na América do Norte
-xlim <- c(-180, -40)  # Ajuste conforme necessário para cobrir a América do Norte
-ylim <- c(10, 80)     # Ajuste conforme necessário para cobrir a América do Norte
+xlim <- c(-180, -60)  # Ajuste conforme necessário para cobrir a América do Norte
+ylim <- c(10, 70)     # Ajuste conforme necessário para cobrir a América do Norte
 
 # Visualizar mapa --------------------------------------------------------------------------------------------------------------------------
 
@@ -75,10 +97,14 @@ ggplot() +
   geom_sf(data = america, fill = "gray80", color = "white") +  # Fronteiras dos países
   geom_sf(data = coords_sf, color = "#123134",
           size = 2) + 
-    geom_sf(data = coords_sf1, color = "#096876", 
+  geom_sf(data = coords_sf1, color = "#096876", 
           size = 2) + 
-      geom_sf(data = coords_sf2, color = "#453256",
+  geom_sf(data = coords_sf2, color = "#453256",
           size = 2) + 
+  geom_sf(data = coords_sf3, color = "brown",
+          size = 2) +
+  geom_sf(data = coords_sf4, color = "forestgreen",
+          size = 2) +
   coord_sf(xlim = xlim, ylim = ylim) +
   labs(title = paste("Distribuição Geográfica de", species_name),
        x = "Longitude",
